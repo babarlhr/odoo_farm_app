9fd14d97fdffe32d9fb1da3865a72f48d5eda0f1
lakip <aronlakip@gmail.com>
2019-04-19 13:12:57 +0300

    wop

--- models/res_partner.py
+++ models/res_partner.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import fields, models
+from odoo import fields, models, api
 
 
 class ResPartner(models.Model):
@@ -13,3 +13,11 @@ class ModeOfPaymnent(models.Model):
     mode_of_payment = fields.Selection(selection=[('mpesa', 'MPESA'),
                                                   ('cash', 'CASH'),
                                                   ('cheque', 'CHEQUE')])
+    eggs_sold = fields.Integer('Eggs Sold')
+    amount_totall = fields.Integer('Amount Paid', compute='_compute_amount')
+
+    @api.depends('eggs_sold')
+    def _compute_amount(self):
+        for this in self:
+            total_sales = this.eggs_sold * 10
+            this.amount_totall = total_sales
